f2ddf4d742bd4480a3667e75a32e9ff622cc16ab
AdarshPatwa1 <adarshpatwaa@gmail.com>
2025-01-05 13:54:04 +0530

    ep3

--- App.js
+++ App.js
@@ -2,29 +2,67 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// ReactElement(Object) => HTML(Browser Understands)
-const parent = React.createElement("div", {id: "parent"},
+// React Element
 
-    React.createElement("div", {id: "child"}, [
-        // React.createElement("h1", {id: "heading"}, "i'm an h1 tag")
+// React.createElement => Object => HTMLElement(render)
 
-            // To create more than one sibling we have to use an array
-            React.createElement("h1", {}, "i'm an h1 tag"), 
-            React.createElement("h2", {}, "i'm an h2 tag")
-         // nested react elemenet
-    ])
-);
+const heading = React.createElement("h1", {id: "heading"}, "React");
 
-console.log(parent);
+//  JSX - it is not HTML in javascript. it looks html-like or xml-like syntax
+// it is transpiled(complied or converted into browser's understanding lang) before it reaches the JS - by parcel - by babel
+
+//JSX =>Babel transpiles it to React.createElement =>ReactElement-JS Object =>HTMLElement(render)
+
+const jsxheading = <h1 id="heading" classname="head">React 2</h1>; // if it is in one line than it is perfect jsx
+
+// const jsxheading = (<h1 id="heading" classname="head">
+    // React 2 
+    //  </h1>); // if it is not in one line than it needs to be under ()
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
+// root.render(heading);
+
+
+
+
+
+
+// React Component
+//1- class based components - old way
+//2- functional components - new way
+
+// React Functional Component -starts with cap letter ex=const Heading
+// const HeadingComponent = () => {
+//     return <h1>React functional component</h1>;
+// };
+
+// const HeadingComponent = () => <h1>React functional component</h1>; // both are same
+
+const Title = () => {
+ <h1>React by me</h1>
+}; // we can use normal function as well arrow function is in trend so we are using it
+
+//component composition
+const HeadingComponent = () => {
+    <div id="container">
+        <Title /> 
+            <h1>React functional component</h1>
+    </div>
+};
+// this <Title /> can be written as this  <Title></Title> as well
+// or you can call {Title()} to use it
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // it is just putting parent(object) inside div's root tag 
+root.render(<HeadingComponent />); //this is how component rendered 
 
-// we wil use JSX so that we can make it easy the above react code for creating tags
 
-// const heading = React.createElement("h1", { id: "heading"}, "Hello World from React!"); // created h1 tag
-
-//         const root = ReactDOM.createRoot(document.getElementById("root")); // created root inside react
-
-//         console.log(heading); //object
-
-//         root.render(heading); // lastly rander the heading in react
+// you can inject any js in jsx - just by using {}
+// const number = 100;
+// const HeadingComponent = () => {
+//     <div id="container"> 
+//         {100 +200} this is how you can inject in js in jsx
+//         {number};
+// same with ReactElement use {jsxheading};
+//             <h1>React functional component</h1>
+//     </div>
+// };
