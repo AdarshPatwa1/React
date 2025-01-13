4756126bb10124c88f8893c8f67a0a06fe3d7f8f
AdarshPatwa1 <adarshpatwaa@gmail.com>
2025-01-13 12:17:17 +0530

    ep10

--- src/components/Restaurantcard.js
+++ src/components/Restaurantcard.js
@@ -13,17 +13,14 @@ const Restaurantcard = (props) => { // some people destructure like in place of 
      } = resData?.info;
      
     return (
-        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200 transition-all " >
             <img 
-            className="res-logo"
+            className="w-[250px] h-[150px] rounded-lg "
             alt="res-logo"
-            src={ CDN_URL
-                 +
-                cloudinaryImageId
-              }
+            src={ CDN_URL + cloudinaryImageId}
               />
-            <h3>{name}</h3> 
-            <h4>{cuisines.join(" ")}</h4>
+            <h3 className="font-bold py-4 text-lg">{name}</h3> 
+            <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} Stars</h4>
             <h4>{costForTwo}</h4>
             <h4>{sla.slaString}</h4>
